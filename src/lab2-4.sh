e9567072dc58cb63e3d1413da1fb6bf8cda5fee6
triots <91826034+triots@users.noreply.github.com>
2021-11-30 22:53:08 +0900

    Update lab2-4.sh

--- src/lab2-4.sh
+++ src/lab2-4.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+echo "Is linux fun? (yes/no)
+read answer
+case $answer in
+      yes | y | Y |Yes |YES)
+        echo "Good";;
+[nN]*)
+        echo "Sorry to hear that";;
+*)  
+        echo "please say yes or no"
+        exit 1;;
+
+
+esac
+exit 0
+
 
