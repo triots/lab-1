c9d2f66c7eb443292b275a4bf00a230be4fb1e5e
triots <91826034+triots@users.noreply.github.com>
2021-11-30 22:47:50 +0900

    Update lab2-3.sh

--- src/lab2-3.sh
+++ src/lab2-3.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+H='echo "scale=2; $2/100" |bc'
+BMI='echo "scale =3; $1 / ($H*$H)"|bc'
+a=18
+b=23.5
+if ['echo"$a <= $BMI" |bc -eq 1] && ['echo "$BMI <$b |bc' -eq 1 ];then
+       echo "normal  weight"
+elif ['echo "BMI < $a" |bc' -eq 1]; then
+
+else
+      echo "overweight"
+
+fi
+
+exit 0
+
+
 
